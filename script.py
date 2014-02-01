013ad8fa26896e58b0ffdc9492f2f8c6cceb9ddb
Toyon <sep@umail.ucsb.edu>
2014-02-01 11:49:36 -0800

    piss off

--- script.py
+++ script.py
@@ -1,15 +1,3 @@
 # All rights reserved, BITCHES!!
 
-# Camera trap script
-
-Read data file
-
-Run analysis
-
-Make table
-make header bold
-
-Make small fig, red line
-
-
-messing this shit up big tyme!!!!
+# I can mess this shit up with the worst of them
